d94a2de42c1cbccbb16c4c22d154e07a44ab7b48
sranjithkumar <ranjith@juhomi.com>
2017-06-29 15:07:36 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ContactList extends React.Component {
+ render() {
+   const people = this.props.contacts
+
+   return <ol>
+     {people.map(person => (
+       <li key={person.name}>{person.name}</li>
+     ))}
+   </ol>
+ }
+}
+
+class App extends Component {
+  render() {
+     return (
+       <div className="App">
+       <ContactList contacts={[
+         { name: 'Anish' },
+         { name: 'Ravi' },
+         { name: 'Ranjith' }
+       ]}/>       
+       </div>
+     );
+   }
+}
+
+export default App;
